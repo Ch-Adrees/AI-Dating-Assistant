616a1ccb939d48b85f0ebaeebccb6ef0fed02b25
zain <alizayn865@gmail.com>
2024-12-25 12:29:34 +0500

    ads configuration

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rizzhub/components/theme.dart';
 import 'package:rizzhub/screens/home.dart';
+import 'package:stack_appodeal_flutter/stack_appodeal_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  appodealInitialize() async{
+    await  Appodeal.initialize(
+      appKey: 'edf0eeb97ff2d940bd1fc71234dc74d80f3eb7d06d96bf97',
+      adTypes: [Appodeal.INTERSTITIAL, Appodeal.REWARDED_VIDEO, Appodeal.BANNER],
+      onInitializationFinished: (errors) => {}
+      );
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -17,6 +30,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeOfApp.appTheme,
-        home: const HomeScreen());
+        home: const HomeScreen()
+        );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:rizzhub/components/custom_app_bar.dart';
 import 'package:rizzhub/components/custom_button.dart';
 import 'package:rizzhub/screens/ice_first_message.dart';
+import 'package:rizzhub/Ads.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,8 +13,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  
   @override
   Widget build(BuildContext context) {
+     final adManager = AdManager(context);
     return Scaffold(
       appBar: const CustomAppBar(),
       body: SafeArea(
@@ -48,6 +51,8 @@ class _HomeScreenState extends State<HomeScreen> {
             const SizedBox(
               height: 10,
             ),
+             TextButton(
+                onPressed: () => adManager.showBanner(), child: Text('Show Bnner')),
           ],
         ),
       )),
